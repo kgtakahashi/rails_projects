40779b979a5081e78e4250d15df2017eed7cc072
前田 翼 <TSUBASA@maeda-no-MacBook-Air.local>
2013-07-29 13:00:52 +0900

    画像も登録できるようにした

--- torch-web/app/models/menter.rb
+++ torch-web/app/models/menter.rb
@@ -1,9 +1,17 @@
 class Menter < ActiveRecord::Base
 	has_one :image, class_name: "MenterImage", dependent: :destroy
 
-	attr_accessible :name, :career, :rate, :password, :password_confirmation
-	attr_accessible :name, :career, :rate, as: :admin
+	validates :password, presence: { on: :create },
+		confirmation: { allow_blank: true }
+
 	attr_accessor :password, :password_confirmation
+	
+	ACCESSIBLE_ATTRS = [ :name, :career, :rate,
+		 :password, :password_confirmation, :image_attributes ]
+
+
+	attr_accessible *ACCESSIBLE_ATTRS
+	accepts_nested_attributes_for :image, allow_destroy: true
 
 	validates :password, presence: { on: :create },
 		confirmation: { allow_blank: true }
